f6769fc2e97cf4eb773fc815ef7331cab86086ae
Javier Lete <javier@eskura21.com>
2019-10-21 20:53:24 +0200

    Estilo con comilla tumbada

--- src/app/simple.component.ts
+++ src/app/simple.component.ts
@@ -3,6 +3,10 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-simple',
     template: '<h2>Componente simple al habla</h2>',
-    styles: [ 'h2 { color: teal; }']
+    styles: [
+        `h2 {
+            color: teal;
+        }`
+    ]
 })
-export class SimpleComponent {}
+export class SimpleComponent { }
