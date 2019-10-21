569136f272c5935f6c2be097c048838ec25aa1b5
Javier Lete <javier@eskura21.com>
2019-10-21 20:51:41 +0200

    Añadido de estilos de simple

--- src/app/simple.component.ts
+++ src/app/simple.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-simple',
-    template: '<h1>Componente simple al habla</h1>'
+    template: '<h2>Componente simple al habla</h2>',
+    styles: [ 'h2 { color: teal; }']
 })
 export class SimpleComponent {}
