73a6e17e10a032c93565d91d53bb56a8eb83f31f
Javier Lete <javier@eskura21.com>
2019-10-22 19:54:32 +0200

    Ejemplos de pipes

--- src/app/simple/simple.component.ts
+++ src/app/simple/simple.component.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
+import { Tarea } from '../tarea';
 
 @Component({
     selector: 'app-simple',
     templateUrl: './simple.component.html',
-    styleUrls: [ './simple.component.css' ]
+    styleUrls: ['./simple.component.css']
 })
 export class SimpleComponent {
     mensaje = 'Mensaje desde SimpleComponent';
 
+    mapeadoPlurales:
+        { [k: string]: string } = { '=0': 'Tarea sin inicializar', '=1': 'Primera tarea', other: 'Tarea número #.' };
+
+    mapeadoTarea: any = {'Vender más': 'ADELANTE', 'Hacer el vago': 'Ejem...', other: 'Pues vale'};
+
+    tarea: Tarea = {
+        id: 1,
+        descripcion: 'Tarea de prueba',
+        fechaFinalizacion: new Date(),
+        importe: 12.345
+    };
+
     cambiarMensaje(texto: string) {
         this.mensaje = texto;
+    }
+
+    cambiarFecha(fecha: Date) {
+        this.tarea.fechaFinalizacion = fecha;
     }
 }
